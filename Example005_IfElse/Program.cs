b2f589101ee8059110604000d9ab486614f72623
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-27 20:47:56 +0400

    .Example005 If Else  has been tested

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "женя")
+{
+    Console.WriteLine("Вау! Это же ЖЕНЯ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
