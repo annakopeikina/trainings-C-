85f97a2d01a65e2903bcbac41f6fb9ef9567da29
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-29 04:07:51 +0400

    Example008_function has been added

---
+++ Example008_Function/Program.cs
@@ -0,0 +1,22 @@
+﻿//Найти индекс первого заданного числа (если такие же числа ещё имеются)
+//одномерного массива array из n элементов, равный find.
+//              0   1   2  3   4   5   6  7   8
+int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+
+}
