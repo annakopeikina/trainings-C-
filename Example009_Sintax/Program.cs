0e008d51820c67323359a6f8ec8a0e93c8959659
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-29 04:35:10 +0400

    Example009 sintax tasks with arrays and functions has been created

---
+++ Example009_Sintax/Program.cs
@@ -0,0 +1,29 @@
+﻿// Упражнения в функциях, методах работы с массивами и синтаксисе
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        //index = index + 1;
+        index++;
+    }
+}
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+
+
+FillArray(array);
+PrintArray(array);
+
+
